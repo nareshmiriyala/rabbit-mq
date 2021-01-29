b623792bbfcf7be6ac8c88ac4b34c6bcbe65daf7
Naresh Miriyala <naresh.miriyala@redenergy.com.au>
2021-01-29 19:53:56 +1100

    First rabbit mq commit

---
+++ src/main/groovy/dell/naresh/rabiitmq/RabiitMqApplication.groovy
@@ -0,0 +1,13 @@
+package dell.naresh.rabiitmq
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class RabiitMqApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(RabiitMqApplication, args)
+	}
+
+}
---
+++ src/test/groovy/dell/naresh/rabiitmq/RabiitMqApplicationTests.groovy
@@ -0,0 +1,13 @@
+package dell.naresh.rabiitmq
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RabiitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
